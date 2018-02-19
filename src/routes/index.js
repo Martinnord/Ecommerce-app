8226beaf923182492bde887958177873d274a3e2
martinnord <martin.nordstrom99@gmail.com>
2018-02-19 17:27:53 +0100

    added a new route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { View } from 'react-native'
-import { NativeRouter, Switch, Route, Link } from 'react-router-native'
+import React from 'react';
+import { View } from 'react-native';
+import { NativeRouter, Switch, Route, Link } from 'react-router-native';
 
-import Signup from './Signup'
-import Login from './Login'
+import Signup from './Signup';
+import Login from './Login';
+import Products from './Products';
 
 export default () => (
   <NativeRouter>
     <Switch>
       <Route exact path="/" component={Signup} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/products" component={Products} />
     </Switch>
   </NativeRouter>
-)
+);
