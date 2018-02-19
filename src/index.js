8753e85b187b784214eb6e4e21ae6b9dd37b7730
martinnord <martin.nordstrom99@gmail.com>
2018-02-19 01:24:55 +0100

    added an endpoint to connect to the server

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import Routes from './routes'
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const client = new ApolloClient({
-  link: new HttpLink({uri: 'http://localhost:4000' }),
+  link: new HttpLink({ uri: 'http://localhost:4000' }),
   cache: new InMemoryCache()
-});
+})
 
-
-export default () => {
+export default () => (
   <ApolloProvider client={client}>
     <Routes />
-  </ApolloProvider>,
-
-
-}
+  </ApolloProvider>
+)
