40e1c218db86077c138c1b97300ce4bcf46e0fbe
martinnord <martin.nordstrom99@gmail.com>
2018-02-20 00:34:48 +0100

    Gave TextFields it's own component

---
+++ src/components/TextField.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { TextInput, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  field: {
+    borderBottomWidth: 1,
+    fontSize: 20,
+    marginBottom: 20
+  }
+});
+
+export default class TextField extends React.PureComponent {
+  onChangeText = text => {
+    const { onChangeText, name } = this.props;
+    onChangeText(name, text);
+  };
+
+  render() {
+    const { value, secureTextEntry, name } = this.props;
+
+    return (
+      <TextInput
+        onChangeText={this.onChangeText}
+        value={value}
+        style={styles.field}
+        placeholder={name}
+        autoCapitalize="none"
+        secureTextEntry={!!secureTextEntry}
+      />
+    );
+  }
+}
