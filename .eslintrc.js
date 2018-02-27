a2c4b9023216dc90bae65f675afdc84505e630bf
martinnord <martin.nordstrom99@gmail.com>
2018-02-27 18:49:33 +0100

    added rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: 'airbnb',
   parser: 'babel-eslint',
   "rules": {
-    "react/jsx-filename-extension": 0
+    "react/jsx-filename-extension": 0,
+    "react/prop-types": 0
   }
 }
