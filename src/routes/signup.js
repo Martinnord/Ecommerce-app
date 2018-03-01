e556a9a0476568a79aba563e608e086590b24779
martinnord <martin.nordstrom99@gmail.com>
2018-03-01 18:47:46 +0100

    UI for signup is done

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { AsyncStorage, View, Text, Button } from 'react-native';
+import {
+  AsyncStorage,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import TextField from '../components/TextField';
@@ -14,6 +23,55 @@ const defaultState = {
   errors: {},
   isSubmitting: false,
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F5F5F5',
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  authWrapper: {
+    backgroundColor: '#FFFFFF',
+    width: 330,
+    padding: 40,
+    borderRadius: 5,
+  },
+  authHeader: {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: '600',
+    paddingBottom: 15,
+  },
+  lineStyle: {
+    borderWidth: 1,
+    borderColor: '#F0F3F7',
+    marginBottom: 15,
+  },
+  buttonContainer: {
+    backgroundColor: '#66AAE2',
+    paddingVertical: 15,
+    borderRadius: 5,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: '700',
+  },
+  authText: {
+    textAlign: 'center',
+    paddingTop: 20,
+    fontWeight: '600',
+  },
+  authRedirect: {
+    textAlign: 'center',
+    color: '#4A90E2',
+    paddingTop: 15,
+    fontWeight: '600',
+  },
+});
 
 class Signup extends React.Component {
   state = {
@@ -70,37 +128,48 @@ class Signup extends React.Component {
     const { errors, values: { name, email, password } } = this.state;
 
     return (
-      <View
-        style={{
-          flex: 1,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <View style={{ width: 200 }}>
-          <TextField
-            value={name}
-            name="name"
-            onChangeText={this.onChangeText}
-          />
-          {errors.email && <Text>{errors.email}</Text>}
-          <TextField
-            value={email}
-            name="email"
-            onChangeText={this.onChangeText}
-          />
-          <TextField
-            value={password}
-            name="password"
-            onChangeText={this.onChangeText}
-            secureTextEntry
-          />
-          <Button title="Signup" onPress={this.submit} />
-          <Text>Already have an account?</Text>
-          <Button title="Login" onPress={this.redirectToLogin} />
-        </View>
-      </View>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+          <View style={styles.authWrapper}>
+            <Text style={styles.authHeader}>Signup</Text>
+            <View style={styles.lineStyle} />
+            <TextField
+              value={name}
+              name="name"
+              onChangeText={this.onChangeText}
+            />
+            {errors.email && <Text>{errors.email}</Text>}
+            <TextField
+              value={email}
+              name="email"
+              returnKeyType="next"
+              keyboardType="email-address"
+              onChangeText={this.onChangeText}
+            />
+            {errors.password && <Text>{errors.password}</Text>}
+            <TextField
+              value={password}
+              name="password"
+              returnKeyType="go"
+              onChangeText={this.onChangeText}
+              secureTextEntry
+            />
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text style={styles.buttonText} onPress={this.submit}>
+                Signup
+              </Text>
+            </TouchableOpacity>
+            <Text style={styles.authText}>Already have an account?</Text>
+            <Text
+              style={styles.authRedirect}
+              title="Signup"
+              onPress={this.redirectToSignup}
+            >
+              Login!
+            </Text>
+          </View>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     );
   }
 }
