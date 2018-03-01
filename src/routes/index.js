f0f5db5df40240c4d164d64ea1bc8a3cd33c4a9a
martinnord <martin.nordstrom99@gmail.com>
2018-03-01 19:33:57 +0100

    added new route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,14 +5,16 @@ import Signup from './Signup';
 import Login from './Login';
 import Products from './Products';
 import CheckToken from './checkToken';
+import CreateProduct from './CreateProduct';
 
 export default () => (
   <NativeRouter>
     <Switch>
-      <Route exact path="/checkToken" component={CheckToken} />
+      <Route exact path="/" component={CheckToken} />
       <Route exact path="/signup" component={Signup} />
-      <Route exact path="/" component={Login} />
+      <Route exact path="/login" component={Login} />
       <Route exact path="/products" component={Products} />
+      <Route exact path="/create-product" component={CreateProduct} />
     </Switch>
   </NativeRouter>
 );
