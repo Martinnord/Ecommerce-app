3696236dfbff7fb8d3b6481eed9b1800ec52fff2
martinnord <martin.nordstrom99@gmail.com>
2018-03-05 14:43:04 +0100

    added UI to this route

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,13 +1,57 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, Image } from 'react-native';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-export default ({ history }) => (
-  <View style={{ padding: 50 }}>
-    <Text>Products!!!</Text>
-    <TouchableOpacity>
-      <Text onPress={() => history.push('/create-product')}>
-        Create product
-      </Text>
-    </TouchableOpacity>
-  </View>
-);
+const Products = ({ data: { products }, loading, history }) => {
+  if (loading || !products) {
+    return null;
+  }
+
+  const productsWithKey = products.map(p => ({
+    ...p,
+    key: p.id,
+  }));
+
+  return (
+    <View>
+      <Text style={{ marginTop: 50 }}>this is the products page</Text>
+      <TouchableOpacity>
+        <Text onPress={() => history.push('/create-product')}>
+          Create product
+        </Text>
+      </TouchableOpacity>
+      <FlatList
+        data={productsWithKey}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.name}</Text>
+            <Text>{item.price}</Text>
+            <Text>{item.seller.name}</Text>
+            <Image
+              style={{ height: 100, width: 100 }}
+              source={{ uri: `http://localhost:4000/${item.imageUrl}` }}
+            />
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const productsQuery = gql`
+  {
+    products {
+      id
+      name
+      description
+      price
+      imageUrl
+      seller {
+        name
+      }
+    }
+  }
+`;
+
+export default graphql(productsQuery)(Products);
